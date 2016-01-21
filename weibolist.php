980b18d8f8661d5c71c58b700c91126df6437868
root <root@localhost.localdomain>
2016-01-21 22:36:32 +0800

    调试获取用户微波功能

--- weibolist.php
+++ weibolist.php
@@ -10,6 +10,11 @@ $uid_get = $c->get_uid();
 $uid = $uid_get['uid'];
 $user_message = $c->show_user_by_id( $uid);//根据ID获取用户等基本信息
 
+$de = $c->home_timeline(1,50,0,10000,0,0);
+var_dump($de);
+$count = count($de['statuses']);
+echo $count;
+echo  $de['statuses'][0]['text'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
