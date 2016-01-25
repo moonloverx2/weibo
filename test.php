1f619dbe90189ad5959e23779183b00133a4fc63
root <root@localhost.localdomain>
2016-01-25 21:59:27 +0800

    test

--- test.php
+++ test.php
@@ -2,12 +2,23 @@
 
 //$url='http://api.weibo.com/2/statuses/home_timeline.json?source=4114245531&count=2';
 
-$url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=22&userip=USERS-IP-ADDRESS&start=0&rsz=6";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_REFERER, "http://http://www.luckyxue.com/"/* Enter the URL of your site here */);
-$body = curl_exec($ch);
-curl_close($ch);
-var_dump($body);
+// $url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=22&userip=USERS-IP-ADDRESS&start=0&rsz=6";
+// $ch = curl_init();
+// curl_setopt($ch, CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($ch, CURLOPT_REFERER, "http://http://www.luckyxue.com/"/* Enter the URL of your site here */);
+// $body = curl_exec($ch);
+// curl_close($ch);
+// var_dump($body);
+
+//ignore_user_abort();//关闭浏览器仍然执行
+//set_time_limit(1000);//让程序一直执行下去
+//$interval=3;//每隔一定时间运行
+// session_write_close();
+
+do{
+echo "1"."<br\>";
+	//file_put_contents("log.log",$msg,FILE_APPEND);//记录日志
+	sleep(1);//等待时间，进行下一次操作。
+}while(true);
 ?>
